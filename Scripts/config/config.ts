a522cd61dde0e50dfe6687298aa8ecdf924c2370
AbleVarghese <able.varghese12@gmail.com>
2016-08-01 18:37:22 -0400

    init Commit

---
+++ Scripts/config/config.ts
@@ -0,0 +1,23 @@
+﻿module config {
+
+    // Scene Constants
+    export class Scene {
+        public static MENU: number = 0;
+        public static SLOT_MACHINE: number = 1;
+        public static GAME_OVER: number = 2;
+    }
+    
+    
+    // Screen Constants
+    export class Screen {
+        public static WIDTH: number = 640;
+        public static HEIGHT: number = 550;
+        public static CENTER_X: number = 420;
+        public static CENTER_Y: number = 240;
+    }
+    
+    // Game Constants
+    export class Game {
+        public static FPS: number = 50;
+    }
+}
